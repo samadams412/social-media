7c61f44fd69f061ebae5df0d2166f7d026fb5b4e
Samuel Adams <samueladams412@gmail.com>
2023-07-17 16:45:46 -0500

    Made changes to MongoDB Auth

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB);
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB_LOCAL);
 
 module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -9,10 +9,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/main.html"))
 })
+
 app.use(routes);
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port http://localhost:${PORT}`);
